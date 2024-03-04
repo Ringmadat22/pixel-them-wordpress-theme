9ec53b571c64b26875d5c4c956f2c11f90f04316
Ring Madat <smithemma329@gmail.com>
2024-03-04 20:34:24 +0300

    main

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom'; 
 import Header from './header';
 import Hero from './Hero';
+import Services from './services'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -9,6 +10,7 @@ root.render(
   <React.StrictMode>
     <Header />
     <Hero />
+    <Services />
   </React.StrictMode>
 );
 
---
+++ my-app/src/services.jsx
@@ -0,0 +1,42 @@
+import "./App.css"
+
+function solution(){
+    return (
+        <>
+            <section className="services-section">
+                <div className="services-container">
+                    <div className="services-main">
+                        <h1 className="services-h1">
+                        We build solutions for your everyday problems.
+                        </h1>
+
+                        <div className="divider" style={{height:"6px",display:"flex",width:"100%",justifyContent:"center",textAlign:"center"}}>
+                            <div style={{width:"70px" ,height:"4px", background:"orange"}}></div>
+                        </div>
+                        
+                        <p className="services-p" style={{marginBottom:"40px"}}>
+                            This is what we do in a nutshell
+                        </p>
+
+                        <p className="services-p">
+                            Creative ut tincidunt nibh, varius cursus nunc. Curabitur molestie, metus vel luctus euismod, mi libero laoreet odio, eu dapibus leo tortor sit amet purus. It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
+                        </p>
+
+                        <div className="services-h2-content">
+                            <h4 className="services-h2">
+                                Working with Pixova has been an experience for a lifetime. I strongly reccommend these guys for their awesome support. Erlich Bachman, Aviato
+                            </h4>
+                        </div>
+
+                        <div className="count-content">
+                            
+                        </div>
+
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default solution
