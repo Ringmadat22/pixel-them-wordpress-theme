2816a21954281ab32cd1bce369cada80f552fa7b
Ring Madat <smithemma329@gmail.com>
2024-03-04 20:19:01 +0300

    main

--- my-app/src/Hero.jsx
+++ my-app/src/Hero.jsx
@@ -7,7 +7,7 @@ function Hero() {
         <div className='hero-container' style={{display:"flex", justifyContent:"center"}}>
           <div className='hero-main' style={{marginTop:"200px", display:"flex", justifyContent:"center", width:"100%"}}>
             <div className="hero-main" >
-              <h1 className="hero-h1" style={{fontSize:"2rem", color:"orange", marginBottom:"40px", textAlign:"center"}}>
+              <h1 className="hero-h1" >
                 WELCOME TO PIXELNOVA LITE
               </h1>
 
@@ -15,15 +15,21 @@ function Hero() {
                 <div style={{width:"50px" ,height:"4px", background:"orange"}}></div>
               </div>
 
-              <h2 className="hero-h2" style={{display:"flex", justifyContent:"center", aligneItems:"center",margin:"20px 0", fontSize:"3.5rem", width:"100%",color:"#fff"}}>
+             
+
+              <div className="hero-h2-content" style={{display:"flex", justifyContent:"center", alignItems:"center", width:"100%"}}>
+
+              
+              <h2 className="hero-h2">
                 Free & Modern One-Page Parallax WordPress Theme
               </h2>
 
-              <p className="hero-p" style={{display:"flex", justifyContent:"center", aligneItems:"center", color:"#fff", fontSize:"20px"}}>
+              </div>
+
+              <p className="hero-p" >
                 Your cool business headline here. You can even insert <span className="hero-span" style={{textDecoration:"underline", padding:"0 10px"}}>
                 HTML here & images</span>. Lorem ipsum dolor sit amet lorem dolor sit amet.
               </p>
-
               <div className="hero-buttons-container" style={{ display:"flex", justifyContent:"center", alignItems:"center", marginTop:"40px"}}>
                 <div className="hero-buttons">
                   <button className="no-background" style={{background:"inherit"}}>
